4a42aa3ba76584a2a3b7f9e3b3dfc148136de0b8
Werenskjold <Rilliden@gmail.com>
2020-06-30 21:07:12 +0900

    Commented World Object

--- AirDefense/worldObject.h
+++ AirDefense/worldObject.h
@@ -13,26 +13,44 @@ class WorldObject
 public:
 
 	// Having to hand name each derived class is unfortunate, but quick
+	/*
+		@param objectType Pass a string identifier of the objectType for quick identification
+		@param position Where the object will be placed in the world
+		@param boundaryRadius Circle bounding radius
+	*/
 	WorldObject(const char* objectType, sf::Vector2f position, float boundaryRadius = 0.f);
 
 	virtual ~WorldObject();
 
+	// Gets if the object is dead
 	const bool isObjectDead();
 
+	// Set the object to be dead
 	void setObjectDead();
 
+	// Get the integer identifier of the object
 	const int getID();
 
+	// Set the integer identifier of the object
 	void setID(int i);
 
+	// Gets the object position
 	const sf::Vector2f getPosition();
 
+	// Gets the string object type of this object
 	const char* getObjectType();
 
+	/*
+		Gets if the object's circle bounding radius intersects another object's
+		@param o Object to be tested against
+		@return Gets a bool of true or false if it does intersect
+	*/
 	bool intersects(WorldObject* o) const;
 
+	// Pure virtual function that must be overriden providing logic for what to do on each update tick
 	virtual void update(World* world, float dt) = 0;
 
+	// Pure virtual function that must be overriden providing logic for what to do on each draw tick
 	virtual void draw(sf::RenderWindow* window) = 0;
 
 protected:
