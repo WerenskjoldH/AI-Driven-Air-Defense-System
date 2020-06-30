27f8e2f6eff174763cbb6cbab7e00cf917976d8a
Werenskjold <Rilliden@gmail.com>
2020-06-30 21:06:45 +0900

    Commented Flying Object

--- AirDefense/flyingObject.h
+++ AirDefense/flyingObject.h
@@ -11,25 +11,45 @@
 class FlyingObject : public WorldObject
 {
 public:
+	/*
+		@param x X coordinate to place the object
+		@param y Y coordinate to place the object
+		@param size The bounding radius of the object
+		@param objectType Type of object given as a string
+		@param targetCity City the object is traveling towards
+	*/
 	FlyingObject(float x, float y, float size, const char* objectType, CityObject * targetCity);
 
 	virtual ~FlyingObject();
 
+	// Pure virtual function that must be overriden providing logic for what to do on each update tick
 	virtual void update(World* world, float dt) = 0;
 
+	// Pure virtual function that must be overriden providing logic for what to do on each draw tick
 	virtual void draw(sf::RenderWindow* window) = 0;
 
+	// Pure virutal function that must be overriden providing logic for how to generate the object's heat signature
 	virtual std::vector<float> generateSignature() = 0;
 
+	// Gets a const char* of the derived object's type
 	const char* getSubObjectType();
 
+	// Gets a boolean saying if the object has reached its destination
 	const bool getReachedDestination();
+
+	/*
+		Set if the object has reached its destination or not
+		@param tf Whether the object has reached its desination or not
+	*/
 	void setReachedDestination(bool tf);
 
+	// Gets if hte object has been destroyed or not
 	const bool getDestroyed();
 
+	// "Destroys" the object in the simulation
 	void destroyObject();
 
+	// Allows for the object to be cleaned up and the memory freed by the object manager
 	void allowDeletion();
 
 protected:
