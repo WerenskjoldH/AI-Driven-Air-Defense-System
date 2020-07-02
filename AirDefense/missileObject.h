a11fc41ed9f2f9f64a3e1603bb70cabdbda644aa
Werenskjold <Rilliden@gmail.com>
2020-07-02 20:37:05 +0900

    Commented Missile Object

--- AirDefense/missileObject.h
+++ AirDefense/missileObject.h
@@ -13,16 +13,32 @@
 class MissileObject : public FlyingObject
 {
 public:
+	/*
+		@param x X coordinate to place the object
+		@param y Y coordinate to place the object
+		@param target Target city for the missile object
+	*/
 	MissileObject(float x, float y, CityObject* target);
 
 	virtual ~MissileObject();
 
+	/*
+		Contains all update logic
+		@param world The world this city object will interact with
+		@param dt the delta time between frames -- this can be static or dynamic
+	*/
 	void update(World* world, float dt);
 
+	/*
+		Contains all rendering logic
+		@param window The RenderWindow that we will be drawing to
+	*/
 	void draw(sf::RenderWindow* window);
 
+	// Set the spawner reference equal to s
 	void setSpawner(ProjectileSpawnerObject* s);
 
+	// Generates the heat signature
 	std::vector<float> generateSignature();
 
 private:
