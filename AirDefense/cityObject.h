87cbc987c52404a6f22633f1407f30e84c49d85b
Werenskjold <Rilliden@gmail.com>
2020-07-02 20:17:08 +0900

    Commented City Object

--- AirDefense/cityObject.h
+++ AirDefense/cityObject.h
@@ -15,19 +15,37 @@
 class CityObject : public WorldObject
 {
 public:
-
+	/*
+		@param x X coordinate to place the object
+		@param y Y coordinate to place the object
+		@param population Population of the city
+	*/
 	CityObject(float x, float y, int population);
 
 	virtual ~CityObject();
 
+	/*
+		@return Gets the population of the city
+	*/
 	int getPopulation() const;
 
+	/*
+		Contains all update logic
+		@param world The world this city object will interact with
+		@param dt the delta time between frames -- this can be static or dynamic
+	*/
 	void update(World* world, float dt);
 
+	/*
+		Contains all rendering logic
+		@param window The RenderWindow that we will be drawing to
+	*/
 	void draw(sf::RenderWindow* window);
 
+	// Gets if the city has been destroyed
 	bool isDestroyed() const;
 
+	// Destroys the city and deals with all logic that depends on the city being "alive"
 	void destroyCity();
 
 private:
