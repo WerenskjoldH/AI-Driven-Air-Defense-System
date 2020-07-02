0feb88a7e1b22c8c78db1c13c87ecdcd8f2a7fe5
Werenskjold <Rilliden@gmail.com>
2020-07-02 21:33:10 +0900

    Commented Plane Object

--- AirDefense/planeObject.h
+++ AirDefense/planeObject.h
@@ -14,14 +14,30 @@
 class PlaneObject : public FlyingObject 
 {
 public:
+
+	/*
+		@param x X coordinate to place the object
+		@param y Y coordinate to place the object
+		@param destinationCity Target city for the plane object
+	*/
 	PlaneObject(float x, float y, CityObject* destinationCity);
 
 	virtual ~PlaneObject();
 
+	/*
+		Contains all update logic
+		@param world The world this city object will interact with
+		@param dt the delta time between frames -- this can be static or dynamic
+	*/
 	void update(World* world, float dt);
 
+	/*
+		Contains all rendering logic
+		@param window The RenderWindow that we will be drawing to
+	*/
 	void draw(sf::RenderWindow* window);
 
+	// Generates a heat signature for the plane object
 	std::vector<float> generateSignature();
 
 private:
